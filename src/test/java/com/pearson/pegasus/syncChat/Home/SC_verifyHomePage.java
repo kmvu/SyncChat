368ca05dd4bd4f33ee4de89fb49eb134d754e4aa
Khang Vu <kmvu91@gmail.com>
2015-04-16 14:30:51 -0700

    Updated Assertion - Khang

--- src/test/java/com/pearson/pegasus/syncChat/Home/SC_verifyHomePage.java
+++ src/test/java/com/pearson/pegasus/syncChat/Home/SC_verifyHomePage.java
@@ -3,6 +3,7 @@ package com.pearson.pegasus.syncChat.Home;
 import com.pearson.pegasus.syncChat.library.common.Common;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 /**
  * Created by KhangVu on 4/13/15.
@@ -10,16 +11,16 @@ import org.testng.annotations.Test;
 public class SC_verifyHomePage extends Common {
 
     public HomeCommon homeCommon = new HomeCommon();
+    private SoftAssert softAssert = new SoftAssert();
 
     @Test
     public void testLoginAsPublisher() throws InterruptedException {
         homeCommon.loginAsPublisherFromHome();
 
-        if (homeCommon.isElementPresent("//span[contains(., 'My Courses and Testbanks')]")) {
-            System.out.println("Successfully located!");
-        } else {
-            System.out.println("Nope!");
-        }
+        /* Checking login success */
+        softAssert.assertTrue(
+                homeCommon.isElementPresent("//span[contains(text(), 'My Courses and Testbanks')]"),
+                "Login not successfull!");
     }
 
     @AfterTest
